b834c33fceeeb22c6841e97cc54409b158b475c2
Opeyemi <eco.ajibade@gmail.com>
2025-09-06 21:15:14 +0100

    alx-listing-app

---
+++ components/common/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CardProps } from '../../interfaces';
+
+const Card: React.FC<CardProps> = ({ /* props */ }) => {
+  return (
+    // Card component JSX
+    <div>
+      {/* Card content */}
+    </div>
+  );
+};
+
+export default Card;
--- components/common/Cards.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { CardProps } from "../../interfaces";
-
-export default function Cards() {
-    return <Cards />
-}
